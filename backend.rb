97b1c7fa906bac0de6656917223c620ca15c798d
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2024-10-09 15:26:24 +0200

    backend functions added

--- backend.rb
+++ backend.rb
@@ -13,11 +13,58 @@ class Backend < Sinatra::Base
     erb :index
   end
 
-  #destroy
-  delete '/api/employees/:id' do
+  #login
+  post '/login' do
     content_type :json
-    result = @db.execute('DELETE FROM employees WHERE id = ?', params['id'])
-    return {result: 'success'}.to_json
+    user = db.execute('SELECT * FROM login WHERE name = ?', params[:name]).first
+  
+    if user && BCrypt::Password.new(user['password']) == params[:password]
+      session[:user_id] = user['id']
+      { result: 'success', message: 'Inloggning lyckades!' }.to_json
+    else
+      { result: 'error', message: 'Felaktigt användarnamn eller lösenord.' }.to_json
+    end
+  end
+  
+  #logout
+  post '/logout' do
+    session.clear
+    { result: 'success', message: 'Utloggning lyckades!' }.to_json
+  end
+  
+# Search for cache
+get '/cache/:id' do
+  content_type :json
+  cache_entry = db.execute('SELECT * FROM cache WHERE id = ?', params[:id]).first
+
+  if cache_entry
+    { result: 'success', data: cache_entry }.to_json
+  else
+    halt 404, { result: 'error', message: 'Cache entry not found.' }.to_json
+  end
 end
+
+  #add cache
+  post '/cache' do
+    content_type :json
+    db.execute('INSERT INTO cache (name, cacheinfo) VALUES (?, ?)', [params[:name], params[:cacheinfo]])
+    { result: 'success', message: 'Cachepost skapad!' }.to_json
+  end
+  
+  #update cache
+  update '/cache/:id' do
+    content_type :json
+    db.execute('UPDATE cache SET cacheinfo = ? WHERE id = ?', [params[:cacheinfo], params[:id]])
+    { result: 'success', message: 'Cachepost uppdaterad!' }.to_json
+  end
+  
+
+  #destroy
+  delete '/cache/:id' do
+  content_type :json
+  db.execute('DELETE FROM cache WHERE id = ?', params[:id])
+  { result: 'success', message: 'Cachepost raderad!' }.to_json
+end
+
   
 end
